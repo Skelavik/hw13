131321d46bb953afd6c932ea2da82d15add0098d
Skelavik <mokhov04@yandex.ru>
2024-10-25 09:42:53 +0300

    f

---
+++ app/post/veiw_post.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+from utils import get_post_all
+
+post_blueprint = Blueprint("post_blueprint", __name__)
+
+@post_blueprint.route("/post")
+def post_page():
+    return render_template('post.html')
+
+
+
+
+
+
