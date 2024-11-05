161cf133b6d0a91ac3ca13744b0b9c7cae713655
Skelavik <mokhov04@yandex.ru>
2024-11-05 22:35:50 +0300

    done veiw post

--- app/post/veiw_post.py
+++ app/post/veiw_post.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, render_template
-from utils import get_post_all
+from utils import get_post_by_pk, get_comments_by_post_id
 
 post_blueprint = Blueprint("post_blueprint", __name__)
 
-@post_blueprint.route("/post")
-def post_page():
-    return render_template('post.html',)
+@post_blueprint.route("/post/<int:x>")
+def post_page(x):
+    data = get_post_by_pk(x)
+    data_comments = get_comments_by_post_id(x)
+    count_comments_under_post = len(data_comments)
+    return render_template('post.html', data=data,
+                           comments=data_comments,
+                           count_comments=count_comments_under_post)
 
 
 
